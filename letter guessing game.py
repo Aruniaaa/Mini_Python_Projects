c22a731db9cdd85340f55e866143e7d12331cfb5
Arunia <charu1234569@gmail.com>
2025-04-27 13:12:10 +0530

    letter guessing game.py

--- letter guessing game.py
+++ letter guessing game.py
@@ -26,30 +26,28 @@ random.shuffle(hint_possibilities)
 hints = hint_possibilities
 
 
-for i in hint_possibilities:
-   hint_str = " ".join(hint_possibilities)
 
-
+hint_str = " ".join(hint_possibilities)
 
 print(f"Some of the letters from these hints are in the code, the code has {len(code)} number of letters, note that letters in the code can repeat: {hint_str}")
 
 
 while True:
-    correct_postions = 0
-    incorrect_postions = 0
+    correct_positions = 0
+    incorrect_positions = 0
     guess = input("Enter your guesses and separate them by a space: ").upper()
     guess_list = guess.split(" ")
     try:
         for i in range(len(code)):
           if guess_list[i] == code[i]:
-              correct_postions += 1
+              correct_positions += 1
           if guess_list[i] != code[i] and guess_list[i] in code:
-             incorrect_postions += 1
-        if correct_postions == len(code):
+             incorrect_positions += 1
+        if correct_positions == len(code):
           print(f"You guessed the code (which was: {code}), it took you {counter} attempts!")
           break
         else:
-          print(f"Correct Positions --> {correct_postions} | Incorrect positions --> {incorrect_postions}")
+          print(f"Correct Positions --> {correct_positions} | Incorrect positions --> {incorrect_positions}")
         counter += 1
     except IndexError:
         print("You guessed more or less letters than given in the code, please try again.")
